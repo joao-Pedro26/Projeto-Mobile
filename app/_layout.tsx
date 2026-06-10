43f358b8fdd16e400e0a9ef0406f77335702f96f
joao-Pedro26 <joao.pf.silva@unesp.br>
2026-06-10 19:15:14 -0300

    feat: add emoji picker components and hooks for task management and address lookup
    - Introduced EmojiList component for displaying a horizontal list of emojis.
    - Created EmojiPicker component for modal selection of emojis.
    - Added EmojiSticker component for draggable and resizable emoji stickers.
    - Implemented useBuscaCep hook for fetching address data based on postal code.
    - Developed useTarefas hook for managing a list of tasks with add and remove functionality.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,13 @@
-import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
   return (
     <>
-      <StatusBar style="light" />
       <Stack>
-        <Stack.Screen
-          name="(tabs)"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen name="+not-found" options={{}} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       </Stack>
+      <StatusBar style="light" />
     </>
   );
 }
